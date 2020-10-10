c3cdf1a8a33e4afc48aac4844d35396104082da1
EunJaePark <design0728@naver.com>
2020-10-10 22:05:35 +0900

    update Day6 - Quiz

---
+++ vanillaJS_Challenge/Quiz/Day6/src/index.js
@@ -0,0 +1,58 @@
+// <⚠️ DONT DELETE THIS ⚠️>
+// import "./styles.css";
+// <⚠️ /DONT DELETE THIS ⚠️>
+
+const select = document.querySelector('select');
+const options = document.querySelectorAll('option');
+
+function answerText(country) {
+    const answer = document.querySelector('.answer');
+    answer.innerText = `I'm from ${country}`;
+}
+
+function reloadPage() {
+    if(localStorage.country) {
+        getLSCountry();
+    }
+}
+
+// function compareCountry(LScountry) {
+//     options.forEach(ele => {
+//         if(LScountry === ele.innerText) {
+//             ele.selected = true;
+//         }
+//     });
+// }
+function reloadSaveCountry(LScountry) {
+    select.value = LScountry;
+}
+
+function getLSCountry() {
+    const LScountry = localStorage.getItem('country');
+    console.log(LScountry);
+    // compareCountry(LScountry);
+    reloadSaveCountry(LScountry);
+    answerText(LScountry);
+}
+
+function saveCountry(countryName) {
+    localStorage.setItem('country', countryName);
+    answerText(countryName);
+}
+
+function selectCountry() {
+    select.addEventListener('change', (country) => {
+        console.log(country);
+        const countryName = country.target.value;
+        if(countryName !== '') {
+            saveCountry(countryName);
+        }
+    });
+}
+
+function init() {
+    selectCountry();
+    reloadPage();
+};
+
+init();
